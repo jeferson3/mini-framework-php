0e280ceeedf4e5e504e9a37a2a8a48ad2f2903d4
jeferson <jefersongomes333@hotmail.com>
2021-04-13 12:48:01 -0300

    migrando roteador

--- routes.php
+++ routes.php
@@ -1,141 +1,31 @@
 <?php
 
-//$routes = array(
-//    "/" => [
-//        "index"=>"HomeController@index",
-//    ],
-//    "dashboard" => [
-//        "index"=>"HomeController@dashboard"
-//    ],
-//    "users" => [
-//        "index"=>"UserController@index",
-//        "create"=>"UserController@create",
-//        "store"=>"UserController@store",
-//        "delete"=>"UserController@delete",
-//    ],
-//    "login" => [
-//        "index" => "AuthController@index",
-//    ],
-//    "register" => [
-//        "index" => "AuthController@create",
-//    ],
-//    "auth" => [
-//        "logout" => "AuthController@logout",
-//        "loginPost" => "AuthController@login",
-//        "registerPost" => "AuthController@register"
-//    ]
-//);
-//$page = isset($_GET['p']) ? $_GET['p'] : '/';
-//$method = isset($_GET['action']) ? $_GET['action'] : null;
-//
-//if (array_key_exists($page, $routes))
-//{
-//    $aux = array();
-//
-//    if (!is_null($method) and !is_null(array_key_exists($method, $routes[$page])))
-//        $aux = explode("@", $routes[$page][$method]);
-//    else
-//        $aux =  explode("@", $routes[$page][array_key_first($routes[$page])]);
-//
-////    dd($aux);
-//    $controller = $aux[0];
-//    $method = $aux[1];
-//
-//    $class = "\App\Controllers\\".$controller;
-//    $class::$method();
-//
-//} else {
-//    \App\traits\Views::load('not_found');
-//}
+use App\Config\Router;
 
-use App\Controllers\AuthController;
-use App\Controllers\HomeController;
-use App\Controllers\ProductController;
-use App\Controllers\UserController;
+Router::get("/", "HomeController@index")->name('home');
 
-use App\Routes\Router as Route;
+Router::get("/auth/login", "AuthController@index")->name('login');
 
-Route::add("/", function (){
-    HomeController::index();
-}, 'get');
+Router::post("/auth/login", "AuthController@login")->name('login.post');
 
-Route::add("/auth/login", function (){
-    AuthController::index();
-});
+Router::get("/auth/register", "AuthController@create")->name('register');
 
-Route::add("/auth/login", function (){
-    AuthController::login();
-}, 'post');
+Router::post("/auth/register", "AuthController@register")->name('register.post');
 
-Route::add("/auth/register", function (){
-    AuthController::create();
-});
+Router::post("/auth/logout", "AuthController@logout")->name('logout');
 
-Route::add("/auth/register", function (){
-    AuthController::register();
-}, 'post');
+Router::get("/admin/dashboard", "HomeController@dashboard")->name('dashboard');
 
-Route::add("/auth/logout", function (){
-    AuthController::logout();
-}, 'post');
+Router::get("/admin/products", "ProductController@index")->name('products');
 
-Route::add("/admin/dashboard", function (){
-    HomeController::dashboard();
-});
+Router::get("/admin/products/create", "ProductController@create")->name('products.create');
 
-Route::add("/admin/products", function (){
-    ProductController::index();
-});
+Router::post("/admin/products/store", "ProductController@store")->name('products.store');
 
-Route::add("/admin/products/create", function (){
-    ProductController::create();
-});
+Router::get("/admin/products/{id}/show", "ProductController@show")->name('products.show');
 
-Route::add("/admin/products/store", function (){
-    ProductController::store();
-}, 'post');
+Router::post("/admin/products/{id}/update", "ProductController@update")->name('products.update');
 
-Route::add("/admin/products/([0-9]*)/show", function ($id){
-    if (isset($id))
-    {
-        ProductController::show($id);
-        exit();
-    }
-    else
-    {
-        header("location: $url");
-        exit();
-    }
-});
+Router::post("/admin/products/{id}/delete", "ProductController@delete")->name('products.delete');
 
-Route::add("/admin/products/([0-9]*)/update", function ($id){
-    if (isset($id))
-    {
-        ProductController::update($id);
-        exit();
-    }
-    else
-    {
-        header("location: $url");
-        exit();
-    }
-}, 'post');
-
-Route::add("/admin/products/([0-9]*)/delete", function ($id){
-    if (isset($id))
-    {
-        ProductController::delete($id);
-        exit();
-    }
-    else
-    {
-        header("location: $url");
-        exit();
-    }
-}, 'post');
-
-Route::pathNotFound(function (){
-    echo "not found";
-});
-
-Route::run("/");
+Router::init();
