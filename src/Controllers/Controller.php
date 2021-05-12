c719a3f73bc986a5478ffff9e5003159e137b056
jeferson <jefersongomes333@hotmail.com>
2021-05-12 19:54:55 -0300

    retirando a obrigatoriedade de assinar o método abstrato

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -5,43 +5,14 @@ namespace App\Controllers;
 use App\traits\Middlewares;
 use App\traits\Views;
 
-abstract class Controller
+class Controller
 {
+
+    /**
+     * Integrating Traits of views and middlewares
+     * All controllers will have access to the Traits
+     */
 
     use Views;
     use Middlewares;
-
-    /**
-     * @return void
-     */
-    abstract public static function index();
-
-    /**
-     * @return void
-     */
-    abstract public static function create();
-
-    /**
-     * @return void
-     */
-    abstract public static function store();
-
-    /**
-     * @param int $id
-     * @return void
-     */
-    abstract public static function show(int $id);
-
-    /**
-     * @param int $id
-     * @return void
-     */
-    abstract public static function update(int $id);
-
-    /**
-     * @param int $id
-     * @return void
-     */
-    abstract public static function delete(int $id);
-
 }
